d09ebc60b887e15ef5c592da1a41ab7533540fd0
ioann.voronov <ioann.voronov@gmail.com>
2017-02-01 18:58:56 +0300

    Added solution file with basic classes

---
+++ Primes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Primes
+{
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("SumOfDigitsForPrimes.txt"))
+            {
+                for (int i = 2; i < 1000000; i++)
+                {
+                    if (PrimesProperties.IsIntegerPrime(i))
+                    {
+                        if (PrimesProperties.GetSumOfDigitsForNumber(i) == 2)
+                            sw.WriteLine(" ");
+                        sw.Write(PrimesProperties.GetSumOfDigitsForNumber(i) + " ");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PrimesUtils/PrimesProperties.cs
@@ -0,0 +1,34 @@
+﻿namespace Primes
+{
+    using System;
+
+    public class PrimesProperties
+    {
+        public static bool IsIntegerPrime(int number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 2; i <= boundary; ++i)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        public static int GetSumOfDigitsForNumber(int number)
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
